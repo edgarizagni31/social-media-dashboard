b1aa71e026d9745e0b4b3166b617cb6825efde35
EdgarIzagni <eom3108@gmail.com>
2020-12-28 17:30:22 -0500

    turn on dark theme

---
+++ main.js
@@ -0,0 +1,28 @@
+document.addEventListener("DOMContentLoaded", () => {
+  let btn = document.getElementById("active-dark");
+  btn.checked = false;
+  const dataDark = document.querySelectorAll("[data-dark]");
+  btn.addEventListener("click", () => {
+    for (let i = 0; i < dataDark.length; i++) {
+      if (
+        dataDark[i].classList[0] === "body" ||
+        dataDark[i].classList[0] === "html"
+      ) {
+        dataDark[i].classList.toggle("bg-dark");
+      }
+      if (
+        dataDark[i].classList[0] === "header__title" ||
+        dataDark[i].classList[0] === "description__title" ||
+        dataDark[i].classList[0] === "data__type"
+      ) {
+        dataDark[i].classList.toggle("title-dark");
+      }
+      if (
+        dataDark[i].classList[0] === "dashboard__social-media" ||
+        dataDark[i].classList[0] === "description__data"
+      ) {
+        dataDark[i].classList.toggle("bg-card-dark");
+      }
+    }
+  });
+});
