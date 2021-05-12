f1c0ccdb2e670626a45da61da36b26ea8f56629e
edgarizagni31 <ochoaEdgar3108@gmail.com>
2021-05-12 18:16:36 -0500

    cypress test

--- cypress/integration/test.spec.js
+++ cypress/integration/test.spec.js
@@ -1,7 +1,15 @@
 /// <reference types="cypress" /> 
+
 describe('dark mode test', () => {
     it('dark mode active', () => {
-        cy.visit('http://127.0.0.1:5500/dist/index.html');
+        cy.visit('/dist/index.html');
+
         cy.get('[data-cy = activeDarkTheme]').click();
+
+        cy.get('[bg-dark]').should('have.class','bg-dark');
+
+        cy.get('[title-dark]').should('have.class', 'title-dark');
+
+        cy.get('[bg-card-dark]').should('have.class', 'bg-card-dark');
     })    
 })
