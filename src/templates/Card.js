7129bda55e8439c02ca799a97f531514b0981db2
edgarizagni31 <ochoaEdgar3108@gmail.com>
2021-05-18 09:50:33 -0500

    rename files

---
+++ src/templates/Card.js
@@ -0,0 +1,36 @@
+'use strict';
+
+import '../styles/card.css';
+
+export const Card = ( borderClass, icon, name, username, followers, { number, increment} ) => {
+    const URL_IMGAGES = './assets/images/';
+    const view = `
+        <article 
+          class="dashboard__social-media ${ borderClass }" 
+          data-cy="bg-card-dark"
+          bg-card-dark
+        >
+          <div>
+            <p class="social-media__user">
+              <img src= ${URL_IMGAGES + icon}  alt= ${ name } />
+              ${ username }
+            </p>
+            <p class="social-media__data">
+              ${ followers }
+              <span class="txt-follow">FOLLOWERS</span>
+            </p>
+            <p 
+                class="social-media__today 
+                ${ increment ? 'icon-up': 'icon-down' }"
+            >
+              <img 
+                src=${ increment ?  './assets/images/icon-up.svg' :  './assets/images/icon-down.svg'}  
+              />
+              ${ number } Today
+            </p>
+          </div>
+        </article>
+    `
+
+    return view;
+}
---
+++ src/templates/Details.js
@@ -0,0 +1,32 @@
+import '../styles/details.css';
+
+export const Details = ( desc, icon, number, { percent, increment } ) => {
+    const URL_IMGAGES = './assets/images/';
+    const view = `
+        <article 
+          class="description__data" 
+          data-cy="bg-card-dark"
+          bg-card-dark
+        >
+            <p 
+              class="data__type" 
+              data-cy="title-dark"
+              title-dark
+            > 
+              ${ desc } 
+            </p>
+            <img src=${ URL_IMGAGES + icon } class="data__icon" />
+            <p class="data__number"> ${ number  }  </p>
+            <p 
+              class="data__percent ${ increment ?  'icon-up' : 'icon-down' } "
+            >
+              <img 
+                src=${ increment ? URL_IMGAGES + 'icon-up.svg' : URL_IMGAGES + 'icon-down.svg' } 
+              />
+              ${ percent }%
+            </p>
+        </article>
+    `
+
+    return view;
+}
