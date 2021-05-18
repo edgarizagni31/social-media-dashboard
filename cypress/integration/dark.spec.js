613c9717b7dbacbbe949e4615a86506de88e88c8
edgarizagni31 <ochoaEdgar3108@gmail.com>
2021-05-17 21:37:08 -0500

    testing dark theme

---
+++ cypress/integration/dark.spec.js
@@ -0,0 +1,15 @@
+/// <reference types="cypress" /> 
+
+describe('dark mode test', () => {
+    it('dark mode active', () => {
+        cy.visit('/dist/index.html');
+
+        cy.get('[data-cy = activeDarkTheme]').click();
+
+        cy.get('[data-cy = bg-dark]').should('have.class','bg-dark');
+
+        cy.get('[data-cy = title-dark]').should('have.class', 'title-dark');
+
+        cy.get('[data-cy = bg-card-dark]').should('have.class', 'bg-card-dark');
+    })    
+})
