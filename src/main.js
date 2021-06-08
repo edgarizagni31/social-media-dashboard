b66c36bdcd5cfca97a83e5424d65755563242813
edgarizagni31 <ochoaEdgar3108@gmail.com>
2021-06-07 22:26:37 -0500

    update imports

--- src/main.js
+++ src/main.js
@@ -1,15 +1,14 @@
 'use strict'
 
 import { handleActiveDark } from './utils/theme.js';
-import { displayTemplates } from './utils/displayTemplates.js';
 
 import './styles/normalize.css';
 import './styles/style.css';
+import './styles/dark.css';
 
 document.addEventListener("DOMContentLoaded", () => {
   const btnActiveDark = document.getElementById('active-dark');
   
-  displayTemplates();
   btnActiveDark.checked = false;
   btnActiveDark.addEventListener('click', handleActiveDark);
 });
