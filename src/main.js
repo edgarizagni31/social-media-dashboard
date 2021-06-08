266b6d1021356a804e020bb46c3b892f287c914f
edgarizagni31 <ochoaEdgar3108@gmail.com>
2021-06-08 10:07:50 -0500

    update active dark event

--- src/main.js
+++ src/main.js
@@ -6,9 +6,4 @@ import './styles/normalize.css';
 import './styles/style.css';
 import './styles/dark.css';
 
-document.addEventListener("DOMContentLoaded", () => {
-  const btnActiveDark = document.getElementById('active-dark');
-  
-  btnActiveDark.checked = false;
-  btnActiveDark.addEventListener('click', handleActiveDark);
-});
+document.getElementById('active-dark').addEventListener('click', handleActiveDark);
