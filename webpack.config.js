9414c6bbfdb61c0474b74f4e77a92ca775780924
edgarizagni31 <ochoaEdgar3108@gmail.com>
2021-06-07 22:25:10 -0500

    update webpack.config

--- webpack.config.js
+++ webpack.config.js
@@ -1,3 +1,4 @@
+// dependecies
 const path = require('path');
 const HtmlWebpackPlugin =  require('html-webpack-plugin');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
@@ -7,73 +8,92 @@ const CssMinimizerWebpackPlugin = require('css-minimizer-webpack-plugin');
 const FontminPlugin = require('fontmin-webpack');
 const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 
+// elements of the webpack configuration
+let rules = [];
+const devServer = {
+    contentBase: path.resolve(__dirname, 'dist/'),
+    compress: true,
+    port: 3000
+}
+
+// rules 
+const rulesJS = {
+    test: /\.js$/,
+    exclude: /(node_modules)/,
+    use: {
+        loader: 'babel-loader'
+    }
+}
+
+const rulesCss = {
+    test: /\.css$/,
+    use: [
+        MiniCssExtractPlugin.loader,
+        'css-loader'
+    ]
+}
+
+const rulesPug = {
+    test:  /\.pug$/,
+    use: [
+        'html-loader',
+        'pug-html-loader'
+    ]
+}
+
+const rulesJpg = {
+    test: /\.(png|jpg)$/,
+    type: 'asset/resource',
+}
+
+const rulesSvg = {
+    test: /\.svg$/,
+    type: 'asset/inline'
+}
+
+const rulesFont = {
+    test: /\.ttf$/,
+    use: {
+        loader: 'url-loader',
+        options: {
+            limit: 10000,
+            minetype: 'application/font-ttf',
+            name: '[name].[contenthash].[ext]',
+            outputPath: './assets/fonts/',
+            publicPath: './assets/fonts/',
+            esModule: true
+        }
+    }
+}
+
+rules.push(rulesPug,rulesCss,rulesJS,rulesJpg,rulesSvg,rulesFont);
+
 module.exports = {
     entry: './src/main.js',
     output: {
         path: path.resolve(__dirname,'dist'),
         filename: 'index.[contenthash].js',
+        assetModuleFilename: "assets/images/[hash][ext]"
     },
     resolve: {
-        extensions: ['js']
+        extensions: ['.js']
     },
-    module: {
-        rules: [
-            {
-                test: /\.js$/,
-                exclude: /node-modules/,
-                use: {
-                    loader: 'babel-loader'
-                }
-            },
-            {
-                test: /\.css$/,
-                use: [ 
-                    MiniCssExtractPlugin.loader,
-                    'css-loader'
-                ]
-            },
-            {
-                test:  /\.(png|svg)$/,
-                type: 'asset/resource'
-            },
-            {
-                test: /\.ttf$/,
-                use: {
-                    loader: 'url-loader',
-                    options: {
-                        limit: 1000,
-                        minetype: 'application/font-ttf',
-                        name: '[name].[contenthash].[ext]',
-                        outputPath: './assets/fonts/',
-                        publicPath: '../assets/fonts/',
-                        esModule: true,
-                    }
-                }
-            }
-        ]
-    },
+    module: { rules },
+    devServer,
     plugins: [
         new HtmlWebpackPlugin({
             inject: true,
-            template: './public/index.html',
+            template: './src/views/index.pug',
             filename: 'index.html'
         }),
         new MiniCssExtractPlugin({
             filename: 'style.[contenthash].css'
         }),
-        new CopyWebpackPlugin({
-            patterns: [
-                {
-                    from: path.resolve(__dirname,'src','assets/images'),
-                    to: 'assets/images'
-                }
-            ]
-        }),
         new FontminPlugin({
-            autodetect: true, // automatically pull unicode characters from CSS
-            glyphs: ['\uf0c8' /* extra glyphs to include */],
+            autodetect: true, 
+            glyphs: ['\uf0c8'],
         }),
-        new CleanWebpackPlugin()
+        new CleanWebpackPlugin(),
     ],
     optimization: {
         minimize: true,
@@ -81,5 +101,6 @@ module.exports = {
             new TercerWebpackPlugin(),
             new CssMinimizerWebpackPlugin()
         ]
-    }
+    },
+    mode: "production"
 }
